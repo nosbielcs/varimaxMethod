8636194a3550ec7246d9cb732d9abb128dba353a
nosbielcs <nosbielcs@bol.com.br>
2012-07-19 21:11:16 -0300

    terceiro arquivo

---
+++ varimax.R
@@ -0,0 +1,29 @@
+function (x, normalize = TRUE, eps = 1e-05) 
+{
+  nc <- ncol(x)
+  if (nc < 2) 
+    return(x)
+  if (normalize) {
+    sc <- sqrt(drop(apply(x, 1L, function(x) sum(x^2))))
+    x <- x/sc
+  }
+  p <- nrow(x)
+  TT <- diag(nc)
+  d <- 0
+  for (i in 1L:1000L) {
+    z <- x %*% TT
+    B <- t(x) %*% (z^3 - z %*% diag(drop(rep(1, p) %*% z^2))/p)
+    sB <- La.svd(B)
+    TT <- sB$u %*% sB$vt
+    dpast <- d
+    d <- sum(sB$d)
+    if (d < dpast * (1 + eps)) 
+      break
+  }
+  z <- x %*% TT
+  if (normalize) 
+    z <- z * sc
+  dimnames(z) <- dimnames(x)
+  class(z) <- "loadings"
+  list(loadings = z, rotmat = TT)
+}
